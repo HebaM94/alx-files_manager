378939a4aa9029c1f4b0801af9f693b5fcbaecd5
Heba <hebamagde@gmail.com>
2024-09-30 13:33:17 +0300

    update

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,11 +2,11 @@ import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
 class AppController {
-  static getStatus(request, response) {
+  static getStatus(_request, response) {
     response.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
   }
 
-  static async getStats(request, response) {
+  static async getStats(_request, response) {
     const users = await dbClient.nbUsers();
     const files = await dbClient.nbFiles();
     response.status(200).json({ users, files });
--- utils/db.js
+++ utils/db.js
@@ -5,7 +5,7 @@ class DBClient {
     const HOST = process.env.DB_HOST || 'localhost';
     const PORT = process.env.DB_PORT || 27017;
     const DATABASE = process.env.DB_DATABASE || 'files_manager';
-    const url = `mongodb://${HOST}:${PORT}/${DATABASE}`;
+    const url = `mongodb://${HOST}:${PORT}`;
     this.client = new MongoClient(url, { useUnifiedTopology: true, useNewUrlParser: true });
     this.client.connect((error) => {
       if (!error) this.db = this.client.db(DATABASE);
