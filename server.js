d50ade73187cad8d3b72a119c9b0183b52232b4e
Heba <hebamagde@gmail.com>
2024-09-30 13:30:59 +0300

    try this update for task 2

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = parseInt(process.env.PORT, 10) || 5000;
 
 app.use(express.json());
-app.use(router);
+app.use('/', router);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
