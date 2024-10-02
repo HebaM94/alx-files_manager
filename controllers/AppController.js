7ea80a3675ecb5c790d44c79607ca7afd336c1e8
Heba <hebamagde@gmail.com>
2024-10-02 23:44:26 +0300

    try

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -5,7 +5,7 @@ class AppController {
   static getStatus(_request, response) {
     const redisStatus = redisClient.isAlive();
     const dbStatus = dbClient.isAlive();
-    response.status(200).json({ redis: redisStatus, db: dbStatus });
+    return response.status(200).json({ redis: redisStatus, db: dbStatus });
   }
 
   static async getStats(_request, response) {
