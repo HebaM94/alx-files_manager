36c9719339e5abf770e19f60aac9b1371a2e87bc
Heba <hebamagde@gmail.com>
2024-10-02 04:37:21 +0300

    try task 8: File data

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -1,6 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
 import { promises as fs } from 'fs';
 import Queue from 'bull';
+import mime from 'mime-types';
 import { ObjectID } from 'mongodb';
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
@@ -255,6 +256,44 @@ class FilesController {
       isPublic: false,
       parentId: file.parentId,
     });
+  }
+
+  static async getFile(request, response) {
+    const token = request.header('X-Token');
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key);
+    if (!userId) {
+      return response.status(401).json({ error: 'Not found' });
+    }
+    const users = dbClient.db.collection('users');
+    const idObj = new ObjectID(userId);
+    const user = await users.findOne({ _id: idObj });
+    if (!user) {
+      return response.status(401).json({ error: 'Not found' });
+    }
+
+    const fileId = request.params.id;
+    const idObjFile = new ObjectID(fileId);
+    const files = dbClient.db.collection('files');
+    const file = await files.findOne({ _id: idObjFile });
+
+    if (!file || (file.userId.toString() !== userId && !file.isPublic)) {
+      return response.status(404).json({ error: 'Not found' });
+    }
+
+    if (file.type !== 'file' && file.type !== 'image') {
+      return response.status(400).json({ error: 'Invalid filetype' });
+    }
+    const filePath = file.localPath;
+    const mimeType = mime.lookup(filePath) || 'application/octet-stream';
+
+    try {
+      const data = await fs.readFile(filePath);
+      response.setHeader('Content-Type', mimeType);
+      return response.status(200).send(data);
+    } catch (error) {
+      return response.status(404).json({ error: ' Not found' });
+    }
   }
 }
 
